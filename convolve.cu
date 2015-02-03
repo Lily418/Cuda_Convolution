8be6c43eea54ee3fea03181dbabb75533cac9170
Lily Hoskin <lilyhoskin@gmail.com>
2015-02-03 13:17:44 +0000

    Added reference implementation of convolve1D

--- convolve.cu
+++ convolve.cu
@@ -23,28 +23,6 @@
 
 #include "cutil_inline.h"
 
-// YOU MAY WANT TO CHANGE THIS
-#define BLOCK_SIZE 1024
-#define GRID_SIZE 1
-
-// DO NOT CHANGE THESE
-#define VECTOR_SIZE 1024
-#define ITERS 100
-void datainit(float*, int);
-
-/*
- * Function to be reduced on the device
- * Eg: sum
- */
-__device__ float d_f(float x, float y) { return x + y; }
-
-/*
- * Function to be reduced on the host
- * Eg: sum
- *
- */
-float h_f(float x, float y) { return x + y; }
-
 /*
  * Calculate the reduce by f of all elements in data_in and
  * store the result at a location of your choice in in data_out.
@@ -56,30 +34,43 @@ float h_f(float x, float y) { return x + y; }
  *
  * THIS YOU NEED TO CHANGE!
  */
-__global__ void reduce(float* data_in, float initial)
+__global__ void convolve(float* data_in, float initial)
 {
   int tx = threadIdx.x;
   int bk = blockIdx.x;
-
-  int x = (bk * BLOCK_SIZE * 2) + (tx * 2);
-
-  data_in[bk * BLOCK_SIZE + tx] = d_f(data_in[x], data_in[x + 1]);
-
-  __syncthreads();
 }
 
 /*
- * Reference CPU implementation
+ * Reference CPU implementation, taken from http://www.songho.ca/dsp/convolution/convolution.html
  */
-float host_reduce(float* data_in, float initial, int size)
+bool convolve1D(float* in, float* out, int dataSize, float* kernel, int kernelSize)
 {
-  float rezult = initial;
+    int i, j, k;
 
-  for (int k=0; k < size; k++)
-      rezult = h_f(rezult, data_in[k]);
+    // check validity of params
+    if(!in || !out || !kernel) return false;
+    if(dataSize <=0 || kernelSize <= 0) return false;
 
-  return rezult;
-}
+    // start convolution from out[kernelSize-1] to out[dataSize-1] (last)
+    for(i = kernelSize-1; i < dataSize; ++i)
+    {
+        out[i] = 0;                             // init to 0 before accumulate
+
+        for(j = i, k = 0; k < kernelSize; --j, ++k)
+            out[i] += in[j] * kernel[k];
+        }
+
+        // convolution from out[0] to out[kernelSize-2]
+        for(i = 0; i < kernelSize - 1; ++i)
+        {
+            out[i] = 0;                             // init to 0 before sum
+
+            for(j = i, k = 0; j >= 0; --j, ++k)
+                out[i] += in[j] * kernel[k];
+            }
+
+            return true;
+        }
 
 /*
  * Main program and benchmarking
@@ -94,84 +85,73 @@ int main(int argc, char** argv)
   cutilSafeCall(cudaGetDeviceProperties(&props, devID));
 
   // allocate host memory
-  unsigned int size     = VECTOR_SIZE;              // YOU MAY WANT TO CHANGE THIS
-  unsigned int mem_size = sizeof(float) * size;     // YOU MAY WANT TO CHANGE THIS
-  float* h_data_in      = (float*)malloc(mem_size); // YOU MAY WANT TO CHANGE THIS
-  float* h_data_out     = (float*)malloc(mem_size); // YOU MAY WANT TO CHANGE THIS
+  float in[5] = {3, 4, 5, 0, 0};
+  float out[5];
+  float k[2] = {2,1};
 
-  printf("Input size : %d\n", VECTOR_SIZE);
-  printf("Initial Grid size  : %d\n", GRID_SIZE);
-  printf("Initial Block size : %d\n", BLOCK_SIZE);
-
-  datainit(h_data_in, size);
+  bool success = convolve1D(in, out, 5, k, 2);
+  printf("%d \n", success);
 
   // allocate device memory
-  float* d_data_in;
-  cutilSafeCall(cudaMalloc((void**) &d_data_in, mem_size));
+  //float* d_data_in;
+  //cutilSafeCall(cudaMalloc((void**) &d_data_in, mem_size));
 
   // copy host memory to device
 
 
   // set up kernel for execution
-  printf("Run %d Kernels.\n\n", ITERS);
-  unsigned int timer = 0;
-  cutilCheckError(cutCreateTimer(&timer));
-  cutilCheckError(cutStartTimer(timer));
+  //printf("Run %d Kernels.\n\n", ITERS);
+  //unsigned int timer = 0;
+  //cutilCheckError(cutCreateTimer(&timer));
+  //cutilCheckError(cutStartTimer(timer));
 
   // execute kernel
-  for (int j = 0; j < ITERS; j++)
-  {
-     cutilSafeCall(cudaMemcpy(d_data_in, h_data_in,
-      mem_size, cudaMemcpyHostToDevice));
+  //for (int j = 0; j < ITERS; j++)
+  //{
+  //  cutilSafeCall(cudaMemcpy(d_data_in, h_data_in,
+  //  mem_size, cudaMemcpyHostToDevice));
 
-     reduce<<<GRID_SIZE, BLOCK_SIZE >>>(d_data_in, 0.0);
-     reduce<<<GRID_SIZE, BLOCK_SIZE / 2>>>(d_data_in, 0.0);
-     reduce<<<GRID_SIZE, BLOCK_SIZE / 4>>>(d_data_in, 0.0);
+//   reduce<<<GRID_SIZE, BLOCK_SIZE >>>(d_data_in, 0.0);
+//   reduce<<<GRID_SIZE, BLOCK_SIZE / 2>>>(d_data_in, 0.0);
+//   reduce<<<GRID_SIZE, BLOCK_SIZE / 4>>>(d_data_in, 0.0);
 
 
      // copy result from device to host
-     cutilSafeCall(cudaMemcpy(h_data_out, d_data_in,
-     mem_size, cudaMemcpyDeviceToHost));
+//   cutilSafeCall(cudaMemcpy(h_data_out, d_data_in,
+//   mem_size, cudaMemcpyDeviceToHost));
 
      // Finish the reduction on the host to avoid the overhead of setting up the kernal for small n
-     h_data_out[0] = host_reduce(h_data_out, 0.0, VECTOR_SIZE / 8);
+     //h_data_out[0] = host_reduce(h_data_out, 0.0, VECTOR_SIZE / 8);
 
-  }
+//  }
 
   // check if kernel execution generated and error
-  cutilCheckMsg("Kernel execution failed");
+//  cutilCheckMsg("Kernel execution failed");
 
   // wait for device to finish
-  cudaThreadSynchronize();
+//  cudaThreadSynchronize();
 
   // stop and destroy timer
-  cutilCheckError(cutStopTimer(timer));
-  double dSeconds = cutGetTimerValue(timer)/(1000.0);
-  double dNumOps = ITERS * size;
-  double gflops = dNumOps/dSeconds/1.0e9;
+//  cutilCheckError(cutStopTimer(timer));
+//  double dSeconds = cutGetTimerValue(timer)/(1000.0);
+//  double dNumOps = ITERS * size;
+//  double gflops = dNumOps/dSeconds/1.0e9;
 
   //Log througput
-  printf("Throughput = %.4f GFlop/s\n", gflops);
-  cutilCheckError(cutDeleteTimer(timer));
+//  printf("Throughput = %.4f GFlop/s\n", gflops);
+//  cutilCheckError(cutDeleteTimer(timer));
 
 
 
   // error check
-  printf("Host reduce   : %.4f\n", host_reduce(h_data_in, 0.0, VECTOR_SIZE));
-  printf("Device reduce : %.4f\n", h_data_out[0]);
+//  printf("Host reduce   : %.4f\n", host_reduce(h_data_in, 0.0, VECTOR_SIZE));
+//  printf("Device reduce : %.4f\n", h_data_out[0]);
 
   // clean up memory
-  free(h_data_in);
-  free(h_data_out);
-  cutilSafeCall(cudaFree(d_data_in));
+//  free(h_data_in);
+//  free(h_data_out);
+//  cutilSafeCall(cudaFree(d_data_in));
 
   // exit and clean up device status
-  cudaThreadExit();
-}
-
-// Allocates a matrix with random float entries.
-void datainit(float* data, int size)
-{
-  for (int i = 0; i < size; ++i)
-    data[i] = rand()/(float)RAND_MAX;
+//  cudaThreadExit();
 }
