38d462a5a4f1a94234a95fa91317d88f6dabc0ed
Lily Hoskin <lilyhoskin@gmail.com>
2015-02-22 01:47:48 +0000

    So so fiddly

--- convolve.cu
+++ convolve.cu
@@ -56,7 +56,7 @@ __global__ void convolve_optimised(float* data_in, float* data_out, float* kerne
     data_out[pos] = 0;
 
     for(int i = 0 + kernelSize - 1; i < kernelSize; i++){
-        data_out[pos] += kernel[i] * data_in[pos - i];
+        data_out[pos] += kernel[i] * data_in[pos + kernelSize - 1 - i];
     }
 
 }
