b07ca4a1f1aa32716c2a22f0739642d6f6198443
Lily Hoskin <lilyhoskin@gmail.com>
2015-02-22 14:31:03 +0000

    Remove if

--- convolve.cu
+++ convolve.cu
@@ -60,11 +60,10 @@ __global__ void convolve_optimised(float* data_in, float* data_out, float* kerne
     int pos = (bk * BLOCK_SIZE) + tx;
 
     data_in_shared[tx] = data_in[pos];
-    if(tx == 0){
-        for(int i = 0; i < kernelSize - 1; i++){
-            data_in_shared[BLOCK_SIZE + i] = data_in[pos + BLOCK_SIZE + i];
-        }
+    for(int i = 0; i < kernelSize - 1; i++){
+        data_in_shared[BLOCK_SIZE + i] = data_in[pos + BLOCK_SIZE + i];
     }
+
 
 
     __syncthreads();
